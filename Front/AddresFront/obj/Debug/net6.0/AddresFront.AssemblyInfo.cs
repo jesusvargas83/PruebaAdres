72ac68df28484d01743f259d760d448e30b0099c
jesusvargas83 <vargasj.banca@gmail.com>
2024-03-25 21:55:40 -0500

    Ajuste

--- Front/AddresFront/obj/Debug/net6.0/AddresFront.AssemblyInfo.cs
+++ Front/AddresFront/obj/Debug/net6.0/AddresFront.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AddresFront")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92c690e5fbdc3159c5087e6a976d66f2ca34ae61")]
 [assembly: System.Reflection.AssemblyProductAttribute("AddresFront")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AddresFront")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
